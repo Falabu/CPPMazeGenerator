b49111f264b44e568811758eaf895910fdad30d4
Kurucz Dávid <kdavid@artificialgroup.com>
2021-05-24 21:16:30 +0200

    Removed std::optional

--- main.cpp
+++ main.cpp
@@ -6,28 +6,19 @@
 int main(int argc, char *argv[]) {
     Args args{argc, argv};
     NcursesDisplay display;
-    std::optional<Randomizer> randomizer;
 
-    if (args.haveSeed()) {
-        randomizer = Randomizer(args.getSeed());
-    } else {
-        randomizer = Randomizer();
-    }
-
-    SettingsGenerator setting{randomizer.value(), args.getWidth(), args.getHeight()};
-
-    MazeGenerator mGenerator(setting.getSetting(), randomizer.value());
+    Randomizer randomizer = args.haveSeed() ? Randomizer(args.getSeed()) : Randomizer();
+    SettingsGenerator setting{randomizer, args.getWidth(), args.getHeight()};
+    MazeGenerator mGenerator(setting.getSetting(), randomizer);
 
     display.init();
 
     auto maze = mGenerator.generate();
 
     display.getMazeDrawer().draw(maze.getMazeStructure());
-
-    display.writeLine("Your seed is: " + randomizer->getSeed());
+    display.writeLine("Your seed is: " + randomizer.getSeed());
 
     display.read();
-
     display.deInit();
 
     return 0;
